99342a45e363b2f5573a9f0b3457498dbf762d4f
NazaManzo <nazamanzo.14@gmail.com>
2024-05-15 15:18:49 -0300

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from './components/header/Header';
+import { ItemListContainer } from './components/ItemListContainer';
+import "./css/main.css"
+
+function App() {
+  return (
+    <>
+      <Header />
+      
+      <ItemListContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const NavBar = () => {
+return (
+<nav className="nav">
+        <ul className="nav-menu">
+                <li className="nav-item"><a className="nav-link" href="#">Inicio</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Smartphones</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">TV</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Electrodomésticos</a></li>
+        </ul>
+</nav>
+)
+}
+

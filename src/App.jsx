bcec1af600c09b2c3ca5c2ad7938c84ce0f5a328
NazaManzo <nazamanzo.14@gmail.com>
2024-06-03 17:20:04 -0300

    Segunda-pre-entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { Header } from './components/Header/Header';
-import { ItemListContainer } from './components/ItemListContainer';
 import "./css/main.css"
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header/Header"
+import NotFound from "./components/NotFound"
+import { ItemListContainer } from "./components/ItemListContainer"
+import Footer from "./components/Footer";
+import ItemDetailContainer from "./components/ItemDatailContainer";
 
 function App() {
-  return (
-    <>
-      <Header />
 
-      <ItemListContainer />
-    </>
-  );
+  const [numerito, setNumerito] = useState(1);
+
+  return (
+    <BrowserRouter>
+      <Header numerito={numerito} setNumerito={setNumerito} />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />}/>
+        <Route path="/category/:categoryId" element={<ItemListContainer />}/>
+        <Route path="/item/:itemId" element={<ItemDetailContainer />}/>
+        <Route path="/*" element={<NotFound />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -4,10 +4,10 @@ export const NavBar = () => {
 return (
 <nav className="nav">
         <ul className="nav-menu">
-                <li className="nav-item"><a className="nav-link" href="#">Inicio</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Smartphones</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">TV</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Electrodomésticos</a></li>
+                <li className="nav-item"><a className="nav-link" href="http://localhost:5173/">Inicio</a></li>
+                <li className="nav-item"><a className="nav-link" href="http://localhost:5173/tecnologia">Tecnologia</a></li>
+                <li className="nav-item"><a className="nav-link" href="http://localhost:5173/perifericos">Perifericos</a></li>
+                <li className="nav-item"><a className="nav-link" href="http://localhost:5173/accesorios">Accesorios</a></li>
         </ul>
 </nav>
 )

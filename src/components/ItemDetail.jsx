e3de3fce7f018a58378c976c64bf0905a69a4361
NazaManzo <nazamanzo.14@gmail.com>
2024-07-16 16:46:36 -0300

    img

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -7,7 +7,6 @@ export const ItemDetail = ( { producto } ) => {
 
 return (
     <div>
-        <img src={producto.imagen} />
         <h1>{producto.nombre}</h1>
         <p>{producto.descripcion}</p>
         <p>${producto.precio}</p>
